fd9f1ef7c6183966fca5971f85128ba518da2cb3
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-12-05 17:05:24 +0100

    Set up test chart

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import styled from 'styled-components';
-import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import React from "react";
+import styled from "styled-components";
+import { GithubContext } from "../context/context";
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { repos } = React.useContext(GithubContext);
+  return <ExampleChart />;
 };
 
 const Wrapper = styled.div`
