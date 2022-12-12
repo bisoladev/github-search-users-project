df2d105badc1a9758d657012ee5591a591781f7c
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-12-12 14:25:18 +0100

    Created doughnut chart

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -4,7 +4,51 @@ import { GithubContext } from "../context/context";
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
-  return <ExampleChart />;
+  let languages = repos.reduce((total, item) => {
+    const { language } = item;
+    if (!language) return total;
+    if (!total[language]) {
+      total[language] = { label: language, value: 1 };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+      };
+    }
+
+    return total;
+  }, {});
+
+  languages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+  const chartData = [
+    {
+      label: "HTML",
+      value: "13",
+    },
+    {
+      label: "CSS",
+      value: "160",
+    },
+    {
+      label: "Javascript",
+      value: "80",
+    },
+  ];
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        {/* <ExampleChart data={chartData} /> */}
+        <Pie3D data={languages} />
+        <div></div>
+        <Doughnut2D data={chartData} />
+        <div></div>
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
