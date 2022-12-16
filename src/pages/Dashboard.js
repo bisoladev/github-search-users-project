a52cfaa8dfcace26c48df3c32e2a3d640df1e0d2
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-12-16 23:27:31 +0100

    Set up loading component

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,6 +4,15 @@ import loadingImage from "../images/preloader.gif";
 import { GithubContext } from "../context/context";
 const Dashboard = () => {
   const { isLoading } = React.useContext(GithubContext);
+  if (isLoading) {
+    return (
+      <main>
+        <Navbar />
+        <Search />
+        <img src={loadingImage} className="loading-img" alt="Loading" />
+      </main>
+    );
+  }
   return (
     <main>
       <Navbar></Navbar>
